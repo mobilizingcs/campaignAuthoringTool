b82bcb249210f91851dd7b8b3ef8b1cfd6018a53
Tai Pham <taiducpham@ucla.edu>
2013-02-26 19:20:12 -0800

    fixed bugs on submitcampaign

--- submitCampaign.php
+++ submitCampaign.php
@@ -6,20 +6,29 @@
 
     $url = "https://test.ohmage.org/app/campaign/create";
     $ch = curl_init($url);
+    //curl_setopt($ch, CURLOPT_URL, $url); 
     
     $data = array("auth_token" => $_POST["auth_token"],
         "client" => $_POST["client"],
         "running_state" => $_POST["running_state"],
         "privacy_state" => $_POST["privacy_state"],
         "class_urn_list" => $_POST["class_urn_list"],
-        "xml" => "@xmlCampaign.xml");
+        "xml" => "@J:\\www\\xmlCampaign.xml");
 
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-     
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true); // enable tracking
+    
     $response = curl_exec($ch);
+    $headerSent = curl_getinfo($ch, CURLINFO_HEADER_OUT ); // request headers
+    
+    if(curl_errno($ch)){
+        echo 'Curl error: ' . curl_error($ch);
+    }
     curl_close($ch);
 
+    echo $headerSent;
     echo $response;
 ?>
