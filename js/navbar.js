d529cfe0c6fa892d41cab7de4f4f71958b878bc1
taipham <kael136@gmail.com>
2013-08-31 00:45:04 -0700

    update changes

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,11 @@
 $(function() {
-   $('#username').html('<i class="icon-user"></i> Logged in as ' + $.cookie('username'));
-   $('.logoutButton').click(function (e) {
+    var username = "";
+    oh.user.whoami(function(x) {
+                                username = x;
+                                  });
+    //$('#username').html('<i class="icon-user"></i> Logged in as ' + $.cookie('username'));
+    $('#username').html('<i class="icon-user"></i> Logged in as ' + username);
+    $('.logoutButton').click(function (e) {
         if (confirm("Are you sure you want to log out? All unsaved data will be lost.")) {
             $.post("https://test.ohmage.org/app/user/logout", { client: "campaign-webapp", auth_token: $.cookie('authToken')});
             $.removeCookie('authToken');
