82ea4efb0c885b65792a77e77da6173c500e3016
taipham <kael136@gmail.com>
2013-08-31 00:52:50 -0700

    update login

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,18 @@
 $(function() {
     var username = "";
     oh.user.whoami(function(x) { console.log(x) 
-                                 username = x; });
+                                 username = x; 
+                                 $('#username').html('<i class="icon-user"></i> Logged in as ' + username);});
+
     //$('#username').html('<i class="icon-user"></i> Logged in as ' + $.cookie('username'));
-    $('#username').html('<i class="icon-user"></i> Logged in as ' + username);
+    
     $('.logoutButton').click(function (e) {
         if (confirm("Are you sure you want to log out? All unsaved data will be lost.")) {
-            $.post("https://test.ohmage.org/app/user/logout", { client: "campaign-webapp", auth_token: $.cookie('authToken')});
-            $.removeCookie('authToken');
-            $.removeCookie('username');
-            window.location.replace('login.html');
+            //$.post("https://test.ohmage.org/app/user/logout", { client: "campaign-webapp", auth_token: $.cookie('authToken')});
+            //$.removeCookie('authToken');
+            //$.removeCookie('username');
+            //window.location.replace('login.html');
+            oh.logout(function(x){};);
         } else {
             e.preventDefault();
         }
