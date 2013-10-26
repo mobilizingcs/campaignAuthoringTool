6d6c0fff6318698612140e8ad20b027df3bb5244
taipham <kael136@gmail.com>
2013-10-25 19:28:02 -0700

    Cosmetic fixed

--- js/surveyDisplay.js
+++ js/surveyDisplay.js
@@ -108,7 +108,7 @@ function addSurveyToPage(index) {
                             '<input type="checkbox" id="surveyAnytime" value="">' +
                                 
                             //'</label>' +
-                            'Do you want the survey to be taken at anytime? ' +
+                            '   The survey can be taken at any time. ' +
                             '<i class="help-icon icon-question-sign" data-original-title="A boolean value indicating whether or not the survey may be taken at any time. If false, the survey may only be taken when a trigger has fired indicating that the user may now take the survey." rel="tooltip" data-placement="right"></i>' +
                         //'</div>' +
                     '</div>' +                          
