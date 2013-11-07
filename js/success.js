fb31099a2e55042b56912b5c9d1f4725e19be0f5
taipham <kael136@gmail.com>
2013-11-06 20:07:58 -0800

    Get back to index when logging out

--- js/success.js
+++ js/success.js
@@ -2,4 +2,13 @@ $(function() {
    $('.createCampaign').click(function (e) {
         window.location = ('campaign.html');
    });
+
+   $('.logoutButton').unbind('click').click(function (e) {
+        if (confirm("Are you sure you want to log out? All unsaved data will be lost.")) {
+        	window.location.replace('index.html');
+            oh.logout(function(x){});
+        } else {
+            e.preventDefault();
+        }
+   });
 });
