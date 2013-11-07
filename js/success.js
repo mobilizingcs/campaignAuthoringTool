eb43bc1a09e76374ef588933b90bb847eabeb75a
taipham <kael136@gmail.com>
2013-11-06 20:15:10 -0800

    fixed

--- js/success.js
+++ js/success.js
@@ -3,9 +3,10 @@ $(function() {
         window.location = ('campaign.html');
    });
 
-   $('.logoutButton').unbind('click').click(function (e) {
+   $('.logoutButton').click(function (e) {
         if (confirm("Are you sure you want to log out? All unsaved data will be lost.")) {
         	window.location.replace('index.html');
+        	jQuery(document).empty();
             oh.logout(function(x){});
         } else {
             e.preventDefault();
