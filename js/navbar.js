f3251eabc49766f1770c9b35306ff7438de70e4a
Steve Nolen <technolengy@gmail.com>
2013-12-10 12:43:15 -0800

    fixing how page reloads when logged out. it actually wasnt working before.  However, this fixes #47 again

--- js/navbar.js
+++ js/navbar.js
@@ -7,18 +7,20 @@ $(function() {
     //$('#username').html('<i class="icon-user"></i> Logged in as ' + $.cookie('username'));
     
     $('.logoutButton').click(function (e) {
-        if (confirm("Are you sure you want to log out? All unsaved data will be lost.")) {
+	e.preventDefault();
+	var theyAreSure = confirm('Are you sure you want to log out? All unsaved data will be lost.');
+        if ( theyAreSure ) {
             /*
               SN - since we don't have a final location for the tool,
               let's look up our location. then we set it to support auto redirect.
             */
             history.pushState({}, '', '/'+(window.location.pathname.split("\/",2))[1]);
-            oh.logout(function(x){});
+            oh.logout(function(){
+		location.reload(true);
+	    });
             localStorage.removeItem('campaignWrapper');
             $.removeCookie('currentCampaign');
             $.removeCookie('currentSurvey');
-        } else {
-            e.preventDefault();
         }
    });
 });
