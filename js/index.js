f1b8a7e051c67c6d251c9214661ad7a79b2e4e34
Steve Nolen <technolengy@gmail.com>
2014-01-13 11:34:25 -0800

    implements auto-generation of campaignurn, fixes #59 #58

--- js/index.js
+++ js/index.js
@@ -14,4 +14,21 @@ $(".hastip").focus(function(){
 $(".hastip").focusout(function(){
         $(this).siblings().tooltip('hide');
 });
+//Automatically generate a campaign urn following best practices for a Mobilize use case
+$("#campaignTitle").keyup(updateUrn);
+$(".classes").change(updateUrn);
+$("#campaignUrn").ready(updateUrn);
+function updateUrn(){
+	var urn_name = $("#campaignTitle").val().replace(/\s/g, "");
+	var urn_class = $(".classes option:selected").val().replace(/urn:class:/g, '');
+	var urn_author = $("#authors").val().replace(/[^a-z0-9\s]/gi, '_');
+	var urn = "urn:campaign:"+urn_class+":"+urn_author+":"+urn_name;
+	$("#campaignUrn").val(urn);
+}
+
+
+// Uncomment this to allow users to edit if they check the box.
+//$("#campaignUrn-edit").click(function(){
+//	$("#campaignUrn").prop("disabled", !$(this).attr('checked'));
+//});
 });
